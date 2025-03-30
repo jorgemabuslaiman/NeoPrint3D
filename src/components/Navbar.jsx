9a1bb116a3b7507b4f643458776265225cb50389
jorge7345 <jorge.abuslaiman@gmail.com>
2025-03-30 18:19:53 -0300

    #Algunos cambios

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-const NavigationBar = () => {
+function NavigationBar () {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to="/">Mi Ecommerce</Navbar.Brand>
+
+        <Navbar.Brand as={Link} to="/">
+        <img src="https://i.pinimg.com/736x/c9/98/0b/c9980b7a33a4dc11a275ae12f6967957.jpg" alt="Logo de Mi Ecommerce" style={{width: "80px", height: "80px" }}
+  />
+</Navbar.Brand>
         <Nav className="ms-auto">
           <Nav.Link as={Link} to="/">Inicio</Nav.Link>
           <Button variant="outline-light" as={Link} to="/login">Login</Button>
